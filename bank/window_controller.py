935da0701383061f16f7459f550b0191eb751fd9
Alexei Bobko <abvbobko@gmail.com>
2021-01-31 21:15:17 +0300

    Add FIO labels and edits

--- bank/window_controller.py
+++ bank/window_controller.py
@@ -1,7 +1,7 @@
 import bank.constants as const
 
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QShortcut, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QFrame
 from PyQt5.QtCore import Qt
 
 import sys
@@ -13,6 +13,7 @@ class MainWindow(QMainWindow):
 
         self.resize(width, height)
         self.setWindowTitle(title)
+        self.__init_items()
         # self.setStyleSheet("background-color: black;")
 
         # set projection label and parameters
@@ -20,8 +21,34 @@ class MainWindow(QMainWindow):
         # self.projection_type_label.setMargin(5)
         # self.projection_type_label.setAttribute(Qt.WA_TranslucentBackground)
 
-    def init_items(self):
-        pass
+    def __init_items(self):
+        # fields = ["Фамилия:", "Имя:", "Отчество:", "Дата рождения"]
+        self.surname_label = QLabel("Фамилия:", self)
+        self.surname_label.move(5, 0)
+        self.surname_edit = QLineEdit(self)
+        self.surname_label.setFrameShape(QFrame.Panel)
+        width, height = self.__get_label_text_pixel_size(self.surname_label)
+        self.surname_edit.move(5 + width + 5, 0)
+
+        self.name_label = QLabel("Имя:", self)
+        self.name_label.move(5, self.surname_edit.height())
+        self.name_edit = QLineEdit(self)
+        self.name_label.setFrameShape(QFrame.Panel)
+        width, height = self.__get_label_text_pixel_size(self.name_label)
+        self.name_edit.move(5 + width + 5, self.surname_edit.height())
+
+        self.patronymic_label = QLabel("Отчество:", self)
+        self.patronymic_label.move(5, self.name_edit.height()*2)
+        self.patronymic_edit = QLineEdit(self)
+        self.patronymic_label.setFrameShape(QFrame.Panel)
+        width, height = self.__get_label_text_pixel_size(self.patronymic_label)
+        self.patronymic_edit.move(5 + width + 5, self.name_edit.height()*2)
+
+
+    @staticmethod
+    def __get_label_text_pixel_size(label):
+        size_object = label.fontMetrics().boundingRect(label.text())
+        return size_object.width(), size_object.height()
 
 
 def except_hook(cls, exception, traceback):
