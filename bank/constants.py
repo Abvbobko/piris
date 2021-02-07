fb8c5e8f4fccaa9992d589440418fd439c205aac
Alexei Bobko <abvbobko@gmail.com>
2021-02-07 20:40:41 +0300

    Fill all comboboxes in window

--- bank/constants.py
+++ bank/constants.py
@@ -2,6 +2,9 @@ WIN_WIDTH = 600
 WIN_HEIGHT = 400
 WIN_TITLE = "BANK"
 
+MODES = {0: "Добавление", 1: "Редактирование"}
+CURRENT_MODE = 0
+
 
 
 
--- bank/window_controller.py
+++ bank/window_controller.py
@@ -1,4 +1,6 @@
 import bank.constants as const
+import bank.db_controller as db_controller
+import bank.creds as creds
 
 from PyQt5 import QtGui, uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QFrame, \
@@ -10,12 +12,46 @@ import os
 
 
 class MainWindow(QMainWindow):
-    def __init__(self, width, height, title=""):
+    def __init__(self, cities, citizenships, disabilities, marital_statuses,
+                 width, height, modes, current_mode=0, title=""):
         super(MainWindow, self).__init__()
         # ToDo: change path from static and move ui to project folder
         ui_path = os.path.abspath('../window_view/window.ui')
         uic.loadUi(ui_path, self)
-        # self.setWindowTitle(title)
+
+        self.window_modes = modes
+        self.window_current_mode = current_mode
+
+        self.mode_combobox.clear()
+        self.mode_combobox.addItems(self.window_modes.values())
+
+        city_names = MainWindow.get_names_from_values(cities)
+        self.cities = cities
+        self.residence_city_combobox.clear()
+        self.residence_city_combobox.addItems(city_names)
+        self.registration_city_combobox.clear()
+        self.registration_city_combobox.addItems(city_names)
+
+        citizenships_names = MainWindow.get_names_from_values(citizenships)
+        self.citizenships = citizenships
+        self.citizenship_combobox.clear()
+        self.citizenship_combobox.addItems(citizenships_names)
+
+        disabilities_names = MainWindow.get_names_from_values(disabilities)
+        self.disabilities = disabilities
+        self.disability_combobox.clear()
+        self.disability_combobox.addItems(disabilities_names)
+
+        marital_statuses_names = MainWindow.get_names_from_values(marital_statuses)
+        self.marital_statuses = marital_statuses
+        self.marital_status_combobox.clear()
+        self.marital_status_combobox.addItems(marital_statuses_names)
+
+        self.setWindowTitle(title)
+
+    @staticmethod
+    def get_names_from_values(values):
+        return [None] + [item[1] for item in values] if values else []
 
     @staticmethod
     def __get_label_text_pixel_size(label):
@@ -28,11 +64,27 @@ def except_hook(cls, exception, traceback):
 
 
 if __name__ == '__main__':
+    db = db_controller.DBController(creds.HOST, creds.USER, creds.PASSWORD, creds.DATABASE)
+    cities = db.get_cities()
+    citizenships = db.get_citizenships()
+    marital_statuses = db.get_marital_status()
+    disabilities = db.get_disabilities()
+
     # enable error messages
     sys.excepthook = except_hook
 
     app = QApplication([])
-    window = MainWindow(const.WIN_WIDTH, const.WIN_HEIGHT, const.WIN_TITLE)
+    window = MainWindow(
+        cities=cities,
+        citizenships=citizenships,
+        disabilities=disabilities,
+        marital_statuses=marital_statuses,
+        width=const.WIN_WIDTH,
+        height=const.WIN_HEIGHT,
+        title=const.WIN_TITLE,
+        modes=const.MODES,
+        current_mode=const.CURRENT_MODE
+    )
     window.show()
     # Start the event loop.
     app.exec_()
